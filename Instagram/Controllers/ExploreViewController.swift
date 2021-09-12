4485719575df2a5dd9298a24a7dc8c73a3313cb7
ericktmr <erickmrtt@outlook.com.br>
2021-09-12 20:24:03 -0300

    Subindo primeira versao do projeto, com Controllers configurados e estrutura de pastas

---
+++ Instagram/Controllers/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by Erick Moraes Teixeira on 12/09/21.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Instagram/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Erick Moraes Teixeira on 12/09/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
